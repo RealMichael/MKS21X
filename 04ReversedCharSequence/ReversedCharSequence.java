7ffee25359868320dd5eed837194f1963b3449bc
Michael Luo <mluo1@stuy.edu>
2018-01-02 15:20:21 -0500

    dda

---
+++ 04ReversedCharSequence/ReversedCharSequence.java
@@ -0,0 +1,35 @@
+public class ReversedCharSequence implements CharSequence{
+   private String data;
+
+    public ReversedCharSequence(String s){
+	data = s;
+	String data2 = "";
+	for(int i = data.length() - 1;i >= 0; i--){
+	    data2 += data.charAt(i);
+	}
+	data = data2;
+ }
+
+
+    public char charAt(int index){
+	return data.charAt(index);
+    }
+
+    
+    public int length(){
+	return data.length();
+    }
+
+    public String toString(){
+	return data;
+    }
+
+    public String subSequence(int start, int end){
+	String result = "";
+	for(int i = start;i < end; i ++){
+	    result += data.charAt(i);
+	}
+	return result;
+    }
+}
+
