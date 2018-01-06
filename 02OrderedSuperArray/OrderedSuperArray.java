949b93af34574bd7ea0ae1b5c6917ad5f668be03
Michael Luo <mluo1@stuy.edu>
2018-01-06 17:18:30 -0500

    Works for all !

--- 02OrderedSuperArray/OrderedSuperArray.java
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -13,7 +13,7 @@ public class OrderedSuperArray extends SuperArray{
   public OrderedSuperArray(String [] ary){
 	  super();
 	  for(int i = 0; i < ary.length; i ++){
-		  set(i,ary[i]);
+	      add(ary[i]);
 	  }
   }
 
@@ -28,37 +28,38 @@ public class OrderedSuperArray extends SuperArray{
     //add to the correct spot.
     //you may still use super.add(index,value)
       
-      	super.add(findIndex(value),value);
+      	super.add(findIndexBinary(value),value);
     return true;
   }
 private int findIndex(String ba){
 	for(int i = 0; i < size(); i ++){
-		if(ba.compareTo(get(i)) > 0){
+		if(ba.compareTo(get(i)) <= 0){
 			return i;
 		}
 	}
 	return 0;
 	}
 
-private int findIndexBinary(String ya){
+    private int findIndexBinary(String ya){
 	int start = 0;
-	int end = size() - 1;
-	while(start != end){
-	if(ya.compareTo(get((start + end) / 2)) > 0){
-		start = ((start + end) / 2) + 1;
+	int end = size() ;
+	if(start > end ){
+	    return 0;
 	}
-	else if(ya.compareTo(get((start + end) / 2)) < 0){
-		end = ((start + end) / 2) - 1;
+	while(start != end ){
+	if(ya.compareTo(get((start + end ) / 2 )) > 0){
+		start = ((start + end ) / 2) + 1 ;
 	}
-	else{
-		start = (start + end) / 2;
-		end = start;
-	
+	else{ 
+		end = ((start + end ) / 2);
 	}
 	
-}
+	}
+	
+    
 return start;
 }
+
 private int indexOfBinary(String ya){
 	int start = 0;
 	int end = size() - 1;
